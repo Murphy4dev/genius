82d9ced785c69e7d9ee66bbf92ecd78da6a1aaba
Murphy <Murphy4dev@hotmail.com>
2026-02-27 23:22:57 +0800

    update gitignore

--- scripts/launch_genuis.sh
+++ scripts/launch_genuis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CURRENT=$(cd "$(dirname "$0")"; pwd)
 ELF_FILE="$CURRENT/../output/genius.elf"
+
 TERMINAL="gnome-terminal"
 QEMU_CMD="qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel $ELF_FILE -monitor none -nographic -serial stdio -s -S"
 # start qemu
