e645f836854f1d384107e7d14c023e9c49c18620
Murphy <Murphy4dev@hotmail.com>
2026-02-27 23:21:04 +0800

    fixed make and launch fail

--- scripts/launch_genuis.sh
+++ scripts/launch_genuis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ELF_FILE="/home/murphy/workspace/genius/output/genius.elf"
+CURRENT=$(cd "$(dirname "$0")"; pwd)
+ELF_FILE="$CURRENT/../output/genius.elf"
 TERMINAL="gnome-terminal"
 QEMU_CMD="qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel $ELF_FILE -monitor none -nographic -serial stdio -s -S"
 # start qemu
